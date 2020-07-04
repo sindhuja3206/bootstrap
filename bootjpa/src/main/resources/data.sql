bae7f235c43eed1ba4f8aaac0bc8f78eb478cdcd
jaanu <sindhujar@gmail.com>
2020-07-05 00:16:20 +0530

    Create data.sql

---
+++ bootjpa/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into alien values(101,'sagarika','Java');
+insert into alien values(102,'Kiran','Android');
+insert into alien values(103,'Pravin','Python');
+insert into alien values(104,'Komal','Java');
+insert into alien values(105,'Rafiq','Android');
+insert into alien values(106,'Raju','ruby');
